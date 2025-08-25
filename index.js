cbd7b656c083ee60e65a2842ffce4487d1638670
shreyasmore52 <shreyasmore52@gmail.com>
2025-08-25 17:47:08 +0530

    Update some file

--- index.js
+++ index.js
@@ -4,15 +4,13 @@ const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
 
 const app = express();
-
 app.use(express.json());
-app.use("/user", userRouter);
-app.use("/course", courseRouter);
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/course", courseRouter);
 
 app.get("/",function(req,res){
     res.sendFile( __dirname + "/public/index.html")
 })
-
-
 
 app.listen(3000);
--- routes/course.js
+++ routes/course.js
@@ -3,17 +3,16 @@ const { Router } = require("express");
 const courseRouter = Router();
 
 
-courseRouter.get("/courses", function(req,res){
-    res.json({
-        message: "This is all courses "
-    })
-})
-
-courseRouter.get("/purchase-getway",function(req,res){
-
+courseRouter.post("/purchase",function(req,res){
     //we expect here user is pay money alredy
     res.json({
             message: "This is dummy payment get-way"
+        })
+})
+
+courseRouter.get("/preview",function(req,res){
+    res.json({
+            message: "Preview endpoint"
         })
 })
 
